3a3a65f0563ed4bea4f7a5cd895a4cbeb931c519
zhouhongxiang <zhhx_87@163.com>
2023-06-05 15:41:56 +0800

    mac divise

--- pkg/discovery/serviceRegistry.go
+++ pkg/discovery/serviceRegistry.go
@@ -1,6 +1,8 @@
 package discovery
 
 import (
+	"fmt"
+
 	"github.com/hashicorp/consul/api"
 )
 
@@ -17,6 +19,35 @@ func (c consulServiceRegistry) Register(serviceInstance ServiceInstance) bool {
 	registration.Port = serviceInstance.GetPort()
 	var tags []string
 	if serviceInstance.IsSecure() {
-
+		tags = append(tags, "secure=true")
+	}else {
+		tags = append(tags, "secure=false")
 	}
+	if serviceInstance.GetMetadata() != nil{
+		for key, value := range serviceInstance.GetMetadata(){
+			tags = append(tags, key+"="+value)
+		}
+	}
+	registration.Tags = tags
+	registration.Address = serviceInstance.GetHost()
+
+	check := new(api.AgentServiceCheck)
+
+	schema := "http"
+	if serviceInstance.IsSecure(){
+		schema = "https"
+	}
+	check.HTTP = fmt.Sprintf("%s://%s:%d/actuator/health", schema, registration.Address, registration.Port)
+	check.Timeout = "5s"
+	check.Interval = "5s"
+	check.DeregisterCriticalServiceAfter = "20s"
+	registration.Check = check
+
+	err := c.client.Agent().ServiceRegister(registration)
+	if err != nil{
+		fmt.Println(err)
+		return false
+	}
+
+	if
 }
